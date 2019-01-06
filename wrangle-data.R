fafd8c6999af843d392c2e6ae8dd151bdbbded3d
Mono33 <louis.mono1@gmail.com>
2019-01-06 17:43:00 +0100

    Fix bad repo

---
+++ wrangle-data.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(readr)
+library(dplyr)
+murders <- read_csv("data/murders.csv")
+murders <- murders %>% mutate(region = factor(region) , rate = total/population * 10^5)
+save(murders,file="rda/murders.rda")
+
+
+
